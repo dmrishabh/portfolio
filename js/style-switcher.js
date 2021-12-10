1140c113a4455ef5320a5d627ff174f04d0d97c2
dmrishabh <mishrarishabh9014@gmail.com>
2021-12-10 13:10:11 +0530

    COLOR SWITCHER UPDATE

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -19,28 +19,54 @@ window.addEventListener("scroll", () => {
 const alternateStyles = document.querySelectorAll(".alternate-style");
 console.log(alternateStyles);
 function setActiveStyle(color) {
+  localStorage.setItem("color", color);
+
+  changeColor();
+}
+function changeColor() {
   alternateStyles.forEach((style) => {
-    if (color === style.getAttribute("title")) {
+    if (localStorage.getItem("color") === style.getAttribute("title")) {
       style.removeAttribute("disabled");
     } else {
       style.setAttribute("disabled", "true");
     }
   });
 }
+// check if color key exist
+if (localStorage.getItem("color") !== null) {
+  changeColor();
+}
 // !----------------------- theme light and dark----------------------- !
 const dayNight = document.querySelector(".day-night");
 
 dayNight.addEventListener("click", () => {
-  dayNight.querySelector("i").classList.toggle("fa-sun");
-  dayNight.querySelector("i").classList.toggle("fa-moon");
-
   document.body.classList.toggle("dark");
-});
-
-window.addEventListener("load", () => {
   if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+  updateIcon();
+});
+// check if theme key exist
+
+function themeMode() {
+  if (localStorage.getItem("theme") !== null) {
+    if (localStorage.getItem("theme") === "light") {
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+    }
+  }
+  updateIcon();
+}
+themeMode();
+function updateIcon() {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.remove("fa-moon");
     dayNight.querySelector("i").classList.add("fa-sun");
   } else {
+    dayNight.querySelector("i").classList.remove("fa-sun");
     dayNight.querySelector("i").classList.add("fa-moon");
   }
-});
+}
